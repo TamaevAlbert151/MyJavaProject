6faf8e7f285ca349573b07386b60ecd50d740164
TamaevAlbert151 <tamaevalbert151@gmail.com>
2025-11-16 22:36:57 +0300

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,10 +1,14 @@
 package com.example;
 
+import java.io.PrintStream;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        // Перенаправляем System.out в UTF-8
+        System.setOut(new PrintStream(System.out, true, "UTF-8"));
+
         System.out.println("Добро пожаловать в Task Manager!");
-        TaskManager taskManager = new TaskManager();
-        taskManager.addTask("Учебное задание на Java");
-        taskManager.printTasks();
+        System.out.println("Список задач:");
+        System.out.println("- Учебное задание на Java");
     }
 }
